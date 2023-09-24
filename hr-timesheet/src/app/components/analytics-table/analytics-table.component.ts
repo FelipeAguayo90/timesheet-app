56e12461e52e675a100660aa1e8b42567fe1141a
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-24 12:58:06 -0600

    nested table creation pt2

--- hr-timesheet/src/app/components/analytics-table/analytics-table.component.ts
+++ hr-timesheet/src/app/components/analytics-table/analytics-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Employee } from 'src/app/interfaces/employee';
 
 @Component({
@@ -6,7 +6,10 @@ import { Employee } from 'src/app/interfaces/employee';
   templateUrl: './analytics-table.component.html',
   styleUrls: ['./analytics-table.component.scss'],
 })
-export class AnalyticsTableComponent {
+export class AnalyticsTableComponent implements OnInit {
+  @Input()
+  departmentId: string | undefined;
+
   weekdays: string[] = [
     'monday',
     'tuesday',
@@ -71,4 +74,22 @@ export class AnalyticsTableComponent {
       wednesday: 5,
     },
   ];
+
+  ngOnInit(): void {
+    this.employees = this.employeeData.filter(
+      (employee) => employee.departmentId === this.departmentId
+    );
+  }
+
+  getTotalHours(employee: Employee): number {
+    return (
+      employee.monday +
+      employee.tuesday +
+      employee.wednesday +
+      employee.thursday +
+      employee.friday +
+      employee.saturday +
+      employee.sunday
+    );
+  }
 }
