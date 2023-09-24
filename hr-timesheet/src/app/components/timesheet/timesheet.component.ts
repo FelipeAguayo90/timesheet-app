e92f2d9e091b7259a55a0186b4ddae5b44abd2f0
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-23 19:43:02 -0600

    reactive form use pt2

--- hr-timesheet/src/app/components/timesheet/timesheet.component.ts
+++ hr-timesheet/src/app/components/timesheet/timesheet.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Department } from 'src/app/interfaces/department';
+import { Employee } from 'src/app/interfaces/employee';
 import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
@@ -11,6 +13,9 @@ import { DepartmentsService } from 'src/app/services/departments.service';
 export class TimesheetComponent implements OnInit {
   departments: Department[];
   department: Department;
+  employeeNameFC = new FormControl('');
+  employees: Employee[] = [];
+  employeeId = 0;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,5 +27,20 @@ export class TimesheetComponent implements OnInit {
     this.department = this.departments.find(
       (department) => department.id === this.route.snapshot.params['id']
     );
+  }
+
+  addEmployee(): void {
+    if (this.employeeNameFC.value) {
+      this.employeeId++;
+
+      this.employees.push({
+        id: this.employeeId.toString(),
+        departmentId: this.department?.id,
+        name: this.employeeNameFC.value,
+        payRate: Math.floor(Math.random() * 50) + 50,
+      });
+
+      this.employeeNameFC.setValue('');
+    }
   }
 }
