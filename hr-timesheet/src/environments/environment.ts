810d1d23d1cb7d6db6d92638d87a7ac5d8104315
Felipe Aguayo <felipeaguayo212@icloud.com>
2023-09-24 16:30:51 -0600

    firebase Setup

---
+++ hr-timesheet/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDNJ9xIszelwfFlBSsxt6So87Ejc3t4XoA',
+    authDomain: 'timesheet-9dd86.firebaseapp.com',
+    databaseURL: '<databaseURL goes here>',
+    projectId: 'timesheet-9dd86',
+    storageBucket: 'timesheet-9dd86.appspot.com',
+    messagingSenderId: '473722279145',
+    appId: '1:473722279145:web:1df1257a597a7b12104c78',
+    measurementId: '<measurementId goes here>',
+  },
+};
